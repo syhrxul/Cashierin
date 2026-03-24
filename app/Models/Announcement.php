31faef9238d8ea61b5890c010e58e75bb3f6dd1a
syhrxul <syhrulimtkhan@gmail.com>
2026-03-24 14:42:16 +0700

    announcement edit

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\BelongsToStore;
 
 class Announcement extends Model
 {
-    //
+    use BelongsToStore;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'scope',
+        'store_id',
+        'target_user_ids',
+        'created_by',
+        'is_active',
+        'priority'
+    ];
+
+    protected $casts = [
+        'target_user_ids' => 'array',
+        'is_active' => 'boolean',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
